f6b7cfecc5dcac081c6f439989840892fa7a8d27
lina ALbaroudi <linaALbaroudi@hotmail.com>
2021-12-03 00:42:03 +0300

    .

--- lib/services/heroes_api.dart
+++ lib/services/heroes_api.dart
@@ -7,8 +7,7 @@ import 'package:http/http.dart' as http;
 Future<HeroResponse> searchHero(String searchText, String sortBy) async {
   HeroResponse heroResponse = HeroResponse(status: "404", heroesList: []);
   await http.post(
-    Uri.https('hydrojeenapp.herokuapp.com','/api/v1/users/addnewdevice'),
-    //Uri.parse('https://hydrojeenapp.herokuapp.com/api/v1/users/addnewdevice'),
+    Uri.https('hlocalhost','/heroes_api/search.php'),
     body: {
       'searchText': searchText,
       'sortBy': sortBy
